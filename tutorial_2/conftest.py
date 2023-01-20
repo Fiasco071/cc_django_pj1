8fd0f959177db0d4fd993caecb3ada6868127706
Fiasco071 <stevech071@gmail.com>
2023-01-20 11:52:34 -0800

    test

---
+++ tutorial_2/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tutorial_2.users.models import User
+from tutorial_2.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
